bdc41ddd463353b225e2deffa841b6c329f16e97
Ryan <rrrasti@yahoo.com>
2015-11-24 11:51:41 -0600

    Changed the HTML template for the site

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
+
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # in Python, writing a regex beginning with 'r'
+    # signifies that the regex may have special characters
+    # and that they are intended for the regex pattern itself
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
