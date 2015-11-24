2fcc801f7409843cd027ec672a2a85016a621cc3
Ryan <rrrasti@yahoo.com>
2015-11-24 16:53:21 -0600

    added some custom Django templating logic to display blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # the 'posts' variable becomes the 'QuerySet'
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
